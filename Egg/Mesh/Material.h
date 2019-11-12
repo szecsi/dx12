5392f4b3aac4a9d8992bddb6969479e0d8061237
Laszlo Szecsi <szecsi.laszlo@gmail.com>
2019-11-13 00:56:51 +0100

    material rework works

--- Egg/Mesh/Material.h
+++ Egg/Mesh/Material.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../Common.h"
+#include <map>
 
 namespace Egg {
 	namespace Mesh {
@@ -15,9 +16,26 @@ namespace Egg {
 			D3D12_RASTERIZER_DESC rasterizerState;
 			D3D12_DEPTH_STENCIL_DESC depthStencilState;
 			DXGI_FORMAT dsvFormat;
+
+			com_ptr<ID3D12RootSignatureDeserializer> rsDeserializer;
+			com_ptr<ID3D12ShaderReflection> vsReflection;
+			com_ptr<ID3D12ShaderReflection> gsReflection;
+			com_ptr<ID3D12ShaderReflection> psReflection;
+
+			struct BufferAddressing {
+				D3D12_GPU_VIRTUAL_ADDRESS address;
+				unsigned int perObjectByteStride;
+			};
+			std::map<unsigned int, BufferAddressing > constantBufferBindings;
+			com_ptr<ID3D12DescriptorHeap> srvHeap;
+			unsigned int srvDescriptorTableRootParameterIndex;
+
 		public:
 
-			Material() : rootSignature{ nullptr }, vertexShader{ nullptr }, geometryShader{ nullptr }, pixelShader{ nullptr }, blendState{}, rasterizerState{}, depthStencilState{} {
+			Material() : rootSignature{ nullptr }, vertexShader{ nullptr }, geometryShader{ nullptr }, pixelShader{ nullptr }, blendState{}, rasterizerState{}, depthStencilState{}
+				, rsDeserializer{ nullptr }, vsReflection{ nullptr }, gsReflection{ nullptr }, psReflection{ nullptr },
+				srvHeap{nullptr}
+			{
 				blendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
 				rasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
 				depthStencilState = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
@@ -44,14 +62,31 @@ namespace Egg {
 
 			void SetVertexShader(com_ptr<ID3DBlob> vs) {
 				vertexShader = vs;
+				DX_API("Failed to reflect vertex shader")
+					D3DReflect(vs->GetBufferPointer(), vs->GetBufferSize(), IID_PPV_ARGS(vsReflection.GetAddressOf()));
+
+				DX_API("Failed to deserialize root signature")
+					D3D12CreateRootSignatureDeserializer(vs->GetBufferPointer(), vs->GetBufferSize(), IID_PPV_ARGS(rsDeserializer.GetAddressOf()));
+
 			}
 
 			void SetPixelShader(com_ptr<ID3DBlob> ps) {
 				pixelShader = ps;
+				DX_API("Failed to reflect pixel shader")
+					D3DReflect(ps->GetBufferPointer(), ps->GetBufferSize(), IID_PPV_ARGS(psReflection.GetAddressOf()));
 			}
 
 			void SetGeometryShader(com_ptr<ID3DBlob> gs) {
 				geometryShader = gs;
+				if (gs != nullptr) {
+					DX_API("Failed to reflect geometry shader")
+						D3DReflect(gs->GetBufferPointer(), gs->GetBufferSize(), IID_PPV_ARGS(gsReflection.GetAddressOf()));
+				}
+			}
+
+			void SetSrvHeap(unsigned int srvDescriptorTableRootParameterIndex, com_ptr<ID3D12DescriptorHeap> srvHeap) {
+				this->srvDescriptorTableRootParameterIndex = srvDescriptorTableRootParameterIndex;
+				this->srvHeap = srvHeap;
 			}
 
 			void SetDSVFormat(DXGI_FORMAT format) {
@@ -74,6 +109,69 @@ namespace Egg {
 				psoDesc.SampleMask = UINT_MAX;
 			}
 
+			void ApplyToCommandList(ID3D12GraphicsCommandList* commandList, unsigned int objectIndex = 0) {
+				commandList->SetGraphicsRootSignature(rootSignature.Get());
+				for (auto& binding : constantBufferBindings) {
+					commandList->SetGraphicsRootConstantBufferView(
+						binding.first,
+						binding.second.address +
+						binding.second.perObjectByteStride * objectIndex
+					);
+				}
+				if (srvHeap) {
+					ID3D12DescriptorHeap* descriptorHeaps[] = { srvHeap.Get() };
+					commandList->SetDescriptorHeaps(_countof(descriptorHeaps), descriptorHeaps);
+					commandList->SetGraphicsRootDescriptorTable(srvDescriptorTableRootParameterIndex, srvHeap->GetGPUDescriptorHandleForHeapStart());
+				}
+			}
+
+			template<typename T>
+			void SetConstantBuffer(const T& resource, unsigned int perObjectByteStride = 0, const std::string& nameOverride = "") {
+				std::string name = (nameOverride != "") ? nameOverride.c_str() : typeid(T::Type).name();
+
+				size_t indexOf;
+				if ((indexOf = name.find(' ')) != std::string::npos) {
+					name = name.substr(indexOf + 1);
+				}
+
+				D3D12_SHADER_INPUT_BIND_DESC bindDesc;
+
+				ID3D12ShaderReflection* reflections[] = {
+					vsReflection.Get(), gsReflection.Get(), psReflection.Get()
+				};
+
+				HRESULT hr;
+				for (unsigned int i = 0; i < _countof(reflections); ++i) {
+					if (!reflections[i]) {
+						continue;
+					}
+
+					hr = reflections[i]->GetResourceBindingDescByName(name.c_str(), &bindDesc);
+
+					if (SUCCEEDED(hr)) {
+						break;
+					}
+				}
+
+				ASSERT(SUCCEEDED(hr), "Failed to find constant buffer '%s'\r\nPossible errors:\r\n-Optimized away\r\n-Name mismatch\r\n-Wrong shader used", name.c_str());
+
+				const D3D12_ROOT_SIGNATURE_DESC& rootSignatureDesc = *(rsDeserializer->GetRootSignatureDesc());
+				for (unsigned int i = 0; i < rootSignatureDesc.NumParameters; ++i) {
+					const D3D12_ROOT_PARAMETER& param = rootSignatureDesc.pParameters[i];
+					if (param.ParameterType == D3D12_ROOT_PARAMETER_TYPE_CBV &&
+						param.Descriptor.ShaderRegister == bindDesc.BindPoint &&
+						param.Descriptor.RegisterSpace == bindDesc.Space) {
+						constantBufferBindings[i] = {
+							resource.GetGPUVirtualAddress(),
+							perObjectByteStride
+						};
+						//commandList->SetGraphicsRootConstantBufferView(i, resource.GetGPUVirtualAddress());
+						return;
+					}
+				}
+
+				ASSERT(false, "Failed to bind constant buffer '%s' to root signature.\r\nPossible errors:\r\n-Wrong root signature used on Vertex Shader", name.c_str());
+			}
 
 		GG_ENDCLASS
 
